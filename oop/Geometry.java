61e4658828b18a6ed04955404bc05549fab54bb6
samuelolubukun <samuel.olubukun@binghamuni.edu.ng>
2023-05-06 09:17:15 -0700

    Jframe addition

---
+++ oop/Geometry.java
@@ -0,0 +1,25 @@
+package oop;
+
+public class Geometry extends Maths {
+
+        public Geometry(){
+            System.out.println("constructor called");
+        }
+
+    void accessingPi(){
+        System.out.println(getPi());
+        setPi(34f);
+        System.out.println(getPi());
+    }
+    public float areaOfSquare(float length){
+        return powTwo(length);
+    }
+
+    float areaOfTriangle(float breadth, float length){
+        return divide(1,2) * multiply(breadth, length);
+    }
+
+    float areaOfRectangle(float length, float breadth){
+        return multiply(length, breadth);
+    }
+}
